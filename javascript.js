aaeb652de82e0f9b1449daf87f89440aae4ca939
Khanh <knh.nguyen2002@gmail.com>
2023-06-14 18:34:07 -0500

    Created generate password function and displayed password

--- javascript.js
+++ javascript.js
@@ -1,4 +1,25 @@
+// generate password
+function generate_password(length) {
+    let password = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    length = 16;
+    while (counter < length) {
+      password += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return password
+}
+
+// display password
+window.onload = function display_password(event) {
+    event.preventDefault();
+    let displayPassword = document.querySelector("#password");
+    displayPassword.innerHTML = generate_password();
+}
+
 // show slider amount
 function showAmount(newAmount) {
-    document.getElementById('amount').innerHTML = newAmount;
+    document.getElementById("amount").innerHTML = newAmount;
 }
